4c41494694c9a7dc8c6d91d538b3e2cced0a41fa
RataGaba <ratagada190@gmail.com>
2026-07-07 06:28:00 +0300

    Добавьте файлы проекта.

---
+++ New practica/Form1.cs
@@ -0,0 +1,15 @@
+namespace New_practica;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+
+        Text = "Сортировка Шелла";
+        ClientSize = new Size(680, 600);
+        StartPosition = FormStartPosition.CenterScreen;
+        BackColor = Color.FromArgb(240, 244, 248);
+    }
+}
+
